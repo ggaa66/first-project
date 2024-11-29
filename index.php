52265f2d937859152a06090e2547315d2e696f4c
Alina Gabova <gabovaalina52@gmail.com>
2024-11-29 19:50:37 +0700

    Добавить поле: Year of registration

---
+++ index.php
@@ -0,0 +1,12 @@
+<h2>Добавить объект</h2>
+	<form action="vendor/create.php" method="post">
+	<p>Реестровый номер</p>
+	<input type="text" name="Registration_number">
+	<p>Наименование объекта</p>
+	<input type="text" name="Name_of_the_object">
+	<p>Стоимость</p>
+	<input type="text" name="Cost">
+	<p>Количество</p> 
+	<input type="number" name="Quantity">
+	<button type="submit">Добавить</button>
+	</form>
